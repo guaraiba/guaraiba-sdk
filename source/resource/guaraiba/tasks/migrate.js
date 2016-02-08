4e6f767d1cb10270c88dbf8845ee1cffb2760a9f
Yoandry Pacheco Aguila <yoandrypa@gmail.com>
2016-02-07 21:11:27 -0500

    Fixed bug in migration task.

--- source/resource/guaraiba/tasks/migrate.js
+++ source/resource/guaraiba/tasks/migrate.js
@@ -1,45 +1,51 @@
 namespace('migrate', function () {
     require('./colors');
 
-    var knex = qx.core.BaseInit.getApplication().getDBSchema().getKNex();
+    try {
+        var knex = qx.core.BaseInit.getApplication().getDBSchema().getKNex();
 
-    desc('Creates a new migration, with the (name) of the migration being added.');
-    task('make', { async: true }, function (name) {
-        if (!name) {
-            var promptly = require('promptly'),
-                msg = 'Name of migration in underscore_case:'.prompt,
-                validateName = function (value) {
-                    if (!value.match(/^[a-z][a-z0-9]*([_-][a-z0-9]+)*$/)) {
-                        throw Error('Invalid name for migration file.'.error);
-                    }
+        desc('Creates a new migration, with the (name) of the migration being added.');
+        task('make', { async: true }, function (name) {
+            if (!name) {
+                var promptly = require('promptly'),
+                    msg = 'Name of migration in underscore_case:'.prompt,
+                    validateName = function (value) {
+                        if (!value.match(/^[a-z][a-z0-9]*([_-][a-z0-9]+)*$/)) {
+                            throw Error('Invalid name for migration file.'.error);
+                        }
 
-                    return value;
-                };
+                        return value;
+                    };
 
-            promptly.prompt(msg, { validator: validateName }, function (err, name) {
+                promptly.prompt(msg, { validator: validateName }, function (err, name) {
+                    knex.migrate.make(name).then(function () {
+                        complete();
+                    });
+                });
+            } else {
                 knex.migrate.make(name).then(function () {
                     complete();
                 });
-            });
-        } else {
-            knex.migrate.make(name).then(function () {
+            }
+
+        });
+
+        desc('Runs all migrations that have not yet been run.');
+        task('latest', { async: true }, function () {
+            knex.migrate.latest().then(function (files) {
                 complete();
             });
-        }
-
-    });
-
-    desc('Runs all migrations that have not yet been run.');
-    task('latest', { async: true }, function () {
-        knex.migrate.latest().then(function (files) {
-            complete();
         });
-    });
 
-    desc('Rolls back the latest migration group.');
-    task('rollback', { async: true }, function () {
-        knex.migrate.rollback().then(function () {
-            complete();
+        desc('Rolls back the latest migration group.');
+        task('rollback', { async: true }, function () {
+            knex.migrate.rollback().then(function () {
+                complete();
+            });
         });
-    });
+    } catch (ex) {
+        console.log('----------------------------------------------------------------------');
+        console.log(ex.getComment ? ex.getComment() : ex.toString());
+        console.log('----------------------------------------------------------------------');
+    }
 });
