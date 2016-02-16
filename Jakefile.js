3a8a2844f0491db8d9ec47dfa76a2052792b3622
Yoandry Pacheco Aguila <yoandrypa@gmail.com>
2016-02-15 22:30:49 -0500

    Add bin

--- Jakefile.js
+++ Jakefile.js
@@ -1,9 +1,1 @@
-desc('Generate new guaraiba server application.');
-
-task('default', { async: true }, function () {
-    jake.run('--tasks');
-    console.log('');
-    complete();
-});
-
 require('./source/resource/guaraiba/tasks/generate/application');
---
+++ source/bin/cli.js
@@ -0,0 +1,12 @@
+var path = require('path'),
+    fs = require('fs'),
+    jake = require('jake'),
+    args = process.argv.slice(2);
+
+process.chdir(path.dirname(process.mainModule.filename)+'/../..');
+
+if (args[0] == 'jake') {
+    args.shift()
+}
+
+jake.run.apply(jake, args.length > 0 ? args : ['--tasks']);
