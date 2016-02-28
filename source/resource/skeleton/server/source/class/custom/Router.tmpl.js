e1ec5a80a929bf96ea9d79df603d1c64b22a0577
Yoandry Pacheco Aguila <yoandrypa@gmail.com>
2016-02-28 02:08:25 -0500

    Updated app skeleton.

--- source/resource/skeleton/server/source/class/custom/Router.tmpl.js
+++ source/resource/skeleton/server/source/class/custom/Router.tmpl.js
@@ -1,7 +1,7 @@
 /**
  * Singleton instance class for define any route for application action.
  *
- * @require(${Namespace}.controllers.Test1)
+ * @require(${Namespace}.controllers.Demo)
  */
 qx.Class.define("${Namespace}.Router", {
     type: 'singleton',
@@ -12,9 +12,8 @@ qx.Class.define("${Namespace}.Router", {
          * Initialise the routes for any action.
          */
         init: function () {
-
-            this.get('/').to('${Namespace}.controllers.Test1.index');
-            this.get('/test1/:action(.:format)').to('${Namespace}.controllers.Test1.:action');
+            this.get('/').to(${Namespace}.controllers.Demos, {format: '.html'});
+            this.get('/demos/:action(.:format)').to(${Namespace}.controllers.Demos, ':action');
 
             // BEGIN REGISTER RESOURCE ROUTERS. DON'T REMOVE OR CHANGE THIS COMMENTARY.
             this.resource(${Namespace}.controllers.Peoples);
